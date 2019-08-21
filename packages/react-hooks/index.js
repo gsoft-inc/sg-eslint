1bc456327563dd795fe6bde0b7e0a5c53c9ee92c
Alexandre Asselin <alexandre.asselin@gsoft.com>
2019-08-21 17:20:22 -0400

    remove the shopify plugin from react-hooks

--- packages/react-hooks/index.js
+++ packages/react-hooks/index.js
@@ -3,9 +3,6 @@ module.exports = {
     rules: {
         // https://github.com/facebook/react/tree/master/packages/eslint-plugin-react-hooks
         "react-hooks/exhaustive-deps": "warn",
-        "react-hooks/rules-of-hooks": "warn",
-
-        // https://github.com/Shopify/eslint-plugin-shopify/tree/master/docs/rules
-        "shopify/react-hooks-strict-return": "warn",
+        "react-hooks/rules-of-hooks": "warn"
     }
 };
