a6e0e76e2749b66c0aabc668d4dfc13d3c8df2fd
Alexandre Asselin <alexandre.asselin@gsoft.com>
2019-08-21 17:20:47 -0400

    replace the shopify rule by the one from our plugin

--- packages/strict/index.js
+++ packages/strict/index.js
@@ -1,13 +1,11 @@
 module.exports = {
-    plugins: ["shopify", "imports", "sharegate"],
+    plugins: ["imports", "sharegate"],
     rules: {
-        // https://github.com/Shopify/eslint-plugin-shopify/tree/master/docs/rules
-        "shopify/restrict-full-import": ["warn", ["prop-types"]],
-
         // https://github.com/benmosher/eslint-plugin-import/tree/master/docs/rules
         "import/no-relative-parent-imports": "warn",
 
         // https://github.com/gsoft-inc/sg-eslint/tree/master/packages/plugin/docs/rules
-        "sharegate/prefer-cx": "warn"
+        "sharegate/prefer-cx": "warn",
+        "sharegate/restrict-full-import": ["warn", ["prop-types"]]
     }
 };
