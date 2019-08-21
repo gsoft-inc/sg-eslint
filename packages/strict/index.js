fb45d043d6981e1a6026a9da97059919eceb9e37
Alexandre Asselin <alexandre.asselin@gsoft.com>
2019-08-21 17:10:47 -0400

    added the documentation link

--- packages/strict/index.js
+++ packages/strict/index.js
@@ -7,6 +7,7 @@ module.exports = {
         // https://github.com/benmosher/eslint-plugin-import/tree/master/docs/rules
         "import/no-relative-parent-imports": "warn",
 
+        // https://github.com/gsoft-inc/sg-eslint/tree/master/packages/plugin/docs/rules
         "sharegate/prefer-cx": "warn"
     }
 };
