8211631de95a485d8923e6eaf6b21072b7eeb487
Alexandre Asselin <alexandre.asselin@gsoft.com>
2019-08-22 11:33:14 -0400

    revert test versionning and changed plugin name to @sharegate/eslint-plugin-rules

--- packages/strict/index.js
+++ packages/strict/index.js
@@ -1,11 +1,11 @@
 module.exports = {
-    plugins: ["import", "sharegate"],
+    plugins: ["import", "@sharegate/rules"],
     rules: {
         // https://github.com/benmosher/eslint-plugin-import/tree/master/docs/rules
         "import/no-relative-parent-imports": "warn",
 
         // https://github.com/gsoft-inc/sg-eslint/tree/master/packages/plugin/docs/rules
-        "sharegate/prefer-cx": "warn",
-        "sharegate/restrict-full-import": ["warn", ["prop-types"]]
+        "@sharegate/rules/prefer-cx": "warn",
+        "@sharegate/rules/restrict-full-import": ["warn", ["prop-types"]]
     }
 };
