514817184fc0ef3ba9698d34d021199f465130a2
Alexandre Asselin <alexandre.asselin@gsoft.com>
2021-02-03 11:53:05 -0500

    Added rules to typescript eslint config

--- packages/typescript/index.js
+++ packages/typescript/index.js
@@ -1,7 +1,13 @@
 module.exports = {
     plugins: ["@typescript-eslint"],
     extends: [
+        "plugin:@typescript-eslint/eslint-recommended",
         "plugin:@typescript-eslint/recommended"
     ],
-    parser: "@typescript-eslint/parser"
+    parser: "@typescript-eslint/parser",
+    rules: {
+        "@typescript-eslint/explicit-function-return-type": "off",
+        "@typescript-eslint/explicit-module-boundary-types": "off",
+        "@typescript-eslint/no-use-before-define": ["error", { "functions": false, "classes": true, "variables": false }]
+    }
 };
