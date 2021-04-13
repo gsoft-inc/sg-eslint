c5edfde3ef9ca21d152792dc67677b47b88d66f1
Alexandre Asselin <alexandre.asselin@gsoft.com>
2021-04-13 17:31:32 -0400

    added rules to the typescript config

--- packages/typescript/index.js
+++ packages/typescript/index.js
@@ -14,6 +14,10 @@ module.exports = {
         "no-shadow": "off",
         "@typescript-eslint/no-shadow": "warn",
         "no-use-before-define": "off",
-        "@typescript-eslint/no-use-before-define": ["warn", { "functions": false, "classes": true, "variables": false }]
+        "@typescript-eslint/no-use-before-define": ["warn", { "functions": false, "classes": true, "variables": false }],
+        "@typescript-eslint/member-delimiter-style": ["warn"],
+        "@typescript-eslint/consistent-type-definitions": ["warn"],
+        "@typescript-eslint/no-implicit-any-catch": ["warn"],
+        "@typescript-eslint/explicit-member-accessibility": ["warn", { accessibility: "no-public" }]
     }
 };
