062e8471286532ccf9500d0ba6224670a1ba9d20
LaurentJouanneau <laurent.jouanneau@gsoft.com>
2022-03-29 19:22:34 +0200

    Remove sort-import from typescript package

--- packages/typescript/index.js
+++ packages/typescript/index.js
@@ -2,7 +2,6 @@ module.exports = {
     plugins: ["@typescript-eslint"],
     extends: [
         "@sharegate/eslint-config-recommended",
-        "@sharegate/eslint-config-sort-imports",
         "plugin:@typescript-eslint/eslint-recommended",
         "plugin:@typescript-eslint/recommended"
     ],
