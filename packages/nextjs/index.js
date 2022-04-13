d6478e0a16a771e152d5517a00235a5099f2a50f
Alexandre Asselin <alexandre.asselin@gsoft.com>
2022-04-13 16:30:06 -0400

    modify the config for the nextjs plugin

--- packages/nextjs/index.js
+++ packages/nextjs/index.js
@@ -1,16 +1,6 @@
 module.exports = {
-    parser: "@babel/eslint-parser",
-    plugins: ["next"],
-    extends:["@sharegate/eslint-config-react"],
-    parserOptions: {
-        sourceType: "module",
-        ecmaFeatures: {
-            jsx: true
-        }
-    },
-    settings: {
-        react: {
-            version: "detect"
-        }
-    }
+    extends:[
+        "@sharegate/eslint-config-react",
+        "plugin:@next/next/recommended"
+    ]
 };
